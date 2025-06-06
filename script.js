6557f384da449942069f837d98b1b588db01c977
tonyhalik21 <brovar66@gmail.com>
2025-06-06 13:53:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- KONFIGURACJA ---
 // Zmień ten adres na adres swojego ESP w sieci lokalnej lub na adres DuckDNS
-const espAddress = "http://sterowanieesp12.duckdns.org"; 
+const espAddress = "http://sterowanieesp12.duckdns.org:50001"; 
 // Alternatywnie, jeśli nie używasz DuckDNS z zewnątrz: const espAddress = "http://192.168.X.X";
 
 // --- GLOBALNE ZMIENNE ---
